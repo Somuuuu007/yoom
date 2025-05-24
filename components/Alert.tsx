3628292e198a623a6b49f0192ee940b13f691bfb
Adityalingwal <sanulingwal1@gmail.com>
2025-05-24 23:21:37 +0530

    hi

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -24,7 +24,7 @@ const Alert = ({ title, iconUrl }: PermissionCardProps) => {
               <p className="text-center text-xl font-semibold">{title}</p>
             </div>
 
-            <Button asChild className="bg-black-1">
+            <Button asChild className="bg-pink-1">
               <Link href="/">Back to Home</Link>
             </Button>
           </div>
